398f2c8ef3ca1902ca3f89bc6dceda0a320f2809
02ggang9 <02ggang9@gmail.com>
2023-12-25 15:13:30 +0900

    feat: Section4 Bean의 생명주기 메서드

--- src/main/java/tobySpringBoot/helloboot/HelloController.java
+++ src/main/java/tobySpringBoot/helloboot/HelloController.java
@@ -1,23 +1,26 @@
 package tobySpringBoot.helloboot;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Objects;
 
 
-@Controller
+@RestController
 @RequestMapping("/hello")
 public class HelloController {
 
     private final HelloService helloService;
+    private final ApplicationContext applicationContext;
 
-    public HelloController(HelloService helloService) {
+    public HelloController(HelloService helloService, ApplicationContext applicationContext) {
         this.helloService = helloService;
+        this.applicationContext = applicationContext;
+        System.out.println(applicationContext);
     }
 
     @GetMapping
-    @ResponseBody
     public String hello(@RequestParam(value = "name") String name) {
         // ResponseBody를 붙이지 않거나 RestController를 붙이지 않는다면 반환 값이 String일 때 view를 찾으려고 함.
         // 하지만 View를 만들지 않았으니 404 에러를 발생시킨다.
--- src/main/java/tobySpringBoot/helloboot/HellobootApplication.java
+++ src/main/java/tobySpringBoot/helloboot/HellobootApplication.java
@@ -14,6 +14,7 @@ import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
@@ -27,16 +28,17 @@ import org.springframework.web.servlet.DispatcherServlet;
 import java.io.IOException;
 
 @Configuration
+@ComponentScan
 public class HellobootApplication {
 
     @Bean
-    public HelloController helloController(HelloService helloService) {
-        return new HelloController(helloService);
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
     }
 
     @Bean
-    public HelloService helloService() {
-        return new SimpleHelloService();
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
     }
 
     public static void main(String[] args) {
@@ -46,9 +48,12 @@ public class HellobootApplication {
             protected void onRefresh() {
                 super.onRefresh();
 
-                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+//                dispatcherServlet.setApplicationContext(this);
+
                 WebServer webServer = serverFactory.getWebServer(servletContext -> {
-                    servletContext.addServlet("dispatcherServlet", new DispatcherServlet(this)).addMapping("/*");
+                    servletContext.addServlet("dispatcherServlet", dispatcherServlet).addMapping("/*");
                 });
                 webServer.start();
             }
