c1a6e8da4fc368255d20749428ee5bce5effd465
02ggang9 <02ggang9@gmail.com>
2023-12-25 12:47:33 +0900

    feat: Section3 Hello 컨트롤러 매핑과 바인딩

--- src/main/java/tobySpringBoot/helloboot/HelloController.java
+++ src/main/java/tobySpringBoot/helloboot/HelloController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 /*
  * HTML을 통채로 리턴하는 대신에 응답을 특정한 타입으로 인코딩해서 보내는 방식
  * */
-@RestController
+//@RestController
 public class HelloController {
 
-    @GetMapping("/hello")
+//    @GetMapping("/hello")
     public String hello(String name) {
         return "Hello " + name;
     }
--- src/main/java/tobySpringBoot/helloboot/HellobootApplication.java
+++ src/main/java/tobySpringBoot/helloboot/HellobootApplication.java
@@ -12,6 +12,7 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -29,16 +30,29 @@ public class HellobootApplication {
 
         TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("hello", new HttpServlet() {
+
+            HelloController helloController = new HelloController();
+
+            servletContext.addServlet("frontcontroller", new HttpServlet() {
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                    String name = req.getParameter("name");
+                    // 인증, 보안, 다국어, 공통 기능
+                    if (req.getRequestURL().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+                        String name = req.getParameter("name");
 
-                    resp.setStatus(HttpStatus.OK.value());
-                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-                    resp.getWriter().println("Hello Servlet " + name);
+                        String ret = helloController.hello(name);
+
+                        resp.setStatus(HttpStatus.OK.value());
+                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                        resp.getWriter().println(ret);
+
+                    } else if (req.getRequestURL().equals("/user")) {
+
+                    } else {
+                        resp.setStatus(HttpStatus.NOT_FOUND.value());
+                    }
                 }
-            }).addMapping("/hello");
+            }).addMapping("/*");
         });
         webServer.start();
     }
