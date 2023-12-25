4d433ae8bf6b33a011dc3ecf6474029168f3fca2
02ggang9 <02ggang9@gmail.com>
2023-12-25 13:31:46 +0900

    feat: Section4 스프링 컨테이너 사용

--- src/main/java/tobySpringBoot/helloboot/HellobootApplication.java
+++ src/main/java/tobySpringBoot/helloboot/HellobootApplication.java
@@ -11,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -22,7 +23,12 @@ public class HellobootApplication {
 
     public static void main(String[] args) {
 //		SpringApplication.run(HellobootApplication.class, args);
-        System.out.println("Hello Containerless Standalone Application");
+        GenericApplicationContext applicationContext = new GenericApplicationContext();
+
+        // 메타 데이터를 넣어줌
+        applicationContext.registerBean(HelloController.class);
+        applicationContext.refresh();
+
 
         /*
          * 임베디드 톰켓을 띄워봄
@@ -30,24 +36,18 @@ public class HellobootApplication {
 
         TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-
-            HelloController helloController = new HelloController();
-
             servletContext.addServlet("frontcontroller", new HttpServlet() {
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
                     // 인증, 보안, 다국어, 공통 기능
-                    if (req.getRequestURL().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
                         String name = req.getParameter("name");
 
+                        HelloController helloController = applicationContext.getBean(HelloController.class);
                         String ret = helloController.hello(name);
 
-                        resp.setStatus(HttpStatus.OK.value());
-                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
                         resp.getWriter().println(ret);
-
-                    } else if (req.getRequestURL().equals("/user")) {
-
                     } else {
                         resp.setStatus(HttpStatus.NOT_FOUND.value());
                     }
