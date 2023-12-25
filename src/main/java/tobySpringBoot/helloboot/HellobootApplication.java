5684da78bd772101636207134c3b58000e597d35
02ggang9 <02ggang9@gmail.com>
2023-12-25 12:10:10 +0900

    feat: Section3-1 서블릿 컨테이너 띄우기

--- src/main/java/tobySpringBoot/helloboot/HellobootApplication.java
+++ src/main/java/tobySpringBoot/helloboot/HellobootApplication.java
@@ -1,13 +1,20 @@
 package tobySpringBoot.helloboot;
 
+import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
 
-@SpringBootApplication
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HellobootApplication.class, args);
+//		SpringApplication.run(HellobootApplication.class, args);
+		System.out.println("Hello Containerless Standalone Application");
+
+		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		WebServer webServer = serverFactory.getWebServer();
+		webServer.start();
 	}
 
 }
