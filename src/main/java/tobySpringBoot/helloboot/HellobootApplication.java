e18b36001c370f2075e84df4d3ed6db8d1b902b9
02ggang9 <02ggang9@gmail.com>
2023-12-25 15:20:51 +0900

    feat:Section4 SpringBootApplication

--- src/main/java/tobySpringBoot/helloboot/HellobootApplication.java
+++ src/main/java/tobySpringBoot/helloboot/HellobootApplication.java
@@ -1,36 +1,22 @@
 package tobySpringBoot.helloboot;
 
-import jakarta.servlet.ServletContext;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-import java.io.IOException;
+import static tobySpringBoot.helloboot.MySpringApplication.run;
 
 @Configuration
 @ComponentScan
 public class HellobootApplication {
 
+    // 코드 날리면 동작 안됨.
     @Bean
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
@@ -42,31 +28,9 @@ public class HellobootApplication {
     }
 
     public static void main(String[] args) {
-//		SpringApplication.run(HellobootApplication.class, args);
-        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-            @Override
-            protected void onRefresh() {
-                super.onRefresh();
-
-                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
-                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-//                dispatcherServlet.setApplicationContext(this);
-
-                WebServer webServer = serverFactory.getWebServer(servletContext -> {
-                    servletContext.addServlet("dispatcherServlet", dispatcherServlet).addMapping("/*");
-                });
-                webServer.start();
-            }
-        };
-
-        // 메타 데이터를 넣어줌
-        applicationContext.register(HellobootApplication.class);
-        applicationContext.refresh();
-
-        /*
-         * 임베디드 톰켓을 띄워봄
-         * */
-
+        SpringApplication.run(HellobootApplication.class, args);
+//        MySpringApplication.run(HellobootApplication.class, args);
     }
+
 
 }
