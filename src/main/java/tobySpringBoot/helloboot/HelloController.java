256671bbc610a12675e7fd16655c61a2d851f8fd
02ggang9 <02ggang9@gmail.com>
2023-12-25 12:03:23 +0900

    feat: Section2 스프링 부트 시작하기

---
+++ src/main/java/tobySpringBoot/helloboot/HelloController.java
@@ -0,0 +1,17 @@
+package tobySpringBoot.helloboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+ * HTML을 통채로 리턴하는 대신에 응답을 특정한 타입으로 인코딩해서 보내는 방식
+ * */
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(String name) {
+        return "Hello " + name;
+    }
+
+}
